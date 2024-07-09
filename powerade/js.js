619d807dfa4850f11c2b5c76e45e73f513333bf1
qpitser <tjdwogus112@naver.com>
2024-07-09 14:42:25 +0900

    0709

--- powerade/js.js
+++ powerade/js.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
     $('section').eq(2).mousemove(function(event){
         $('.view').css({
-            left: event.pageX + 15 + 'px',
-            top: event.pageY + 15 + 'px'
-        });
+            left: event.pageX + -50 + 'px',
+            top: event.pageY + -30 + 'px'
+        }).show(); // 마우스를 움직일 때만 보이도록 설정
     });
+
+    $('.view').mouseleave(function(){
+        $(this).hide(); // 마우스가 섹션을 벗어나면 숨기기
+    });
+
+    $('.view').click(function(){
+
+        $('section').eq(2).stop().animate()
+    })
 });
